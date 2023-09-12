150677c5e93719b139185a7076a5c8801d309aad
Ingryd Shirlley <ingryddasilva7@gmail.com>
2023-09-12 09:08:14 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './footer.css'
 import video from '../../Assets/videoFooter.mp4'
 import { FiSend } from 'react-icons/fi'
+import { MdOutlineTravelExplore } from 'react-icons/md'
+import { FaTripadvisor } from 'react-icons/fa'
+import { FiChevronRight } from 'react-icons/fi'
+import { AiFillInstagram, AiFillYoutube, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
@@ -20,8 +24,57 @@ const Footer = () => {
           <div className="inputDiv flex">
             <input type="text" placeholder="Email" />
             <button className="btn fkex" type='submit'>
-              ENVIAR <FiSend className='icon'/>
+              ENVIAR <FiSend className='icon' />
             </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="" className="logo flex"> <MdOutlineTravelExplore className='icon' /> Travel. </a>
+            </div>
+
+            <div className="footerParagraph">
+              Sequência de palavras, geralmente latinas ou alatinadas, utilizada para preencher o
+              espaço destinado a texto numa publicação, página de Internet, interface gráfico, etc.
+              e testar as opções de formatação e edição e o arranjo dos elementos gráficos antes da
+              inserção do conteúdo.
+            </div>
+
+            <div className="footerSocial">
+              <AiOutlineTwitter className='icon' />
+              <AiFillYoutube className='icon' />
+              <FaTripadvisor className='icon' />
+              <AiFillInstagram className='icon' />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">NOSSA AGÊNCIA</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Serviços
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Serviços
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Serviços
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Serviços
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Serviços
+              </li>
+            </div>
           </div>
         </div>
       </div>
